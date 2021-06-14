c204ee3955f39d39fafb6f2d55c15fe23bb451c6
Jarosław Baran <furiousprogramming@gmail.com>
2021-06-15 01:40:38 +0200

    Methods to convert data implemented

--- source/Fairtris.Converter.pp
+++ source/Fairtris.Converter.pp
@@ -24,46 +24,71 @@ var
 
 implementation
 
+uses
+  SysUtils,
+  Fairtris.Memory,
+  Fairtris.Constants;
+
 
 function TConverter.PiecesToString(APieces: Integer): String;
 begin
-  Result := '';
+  Result := '%.3d'.Format([APieces]);
 end;
 
 
 function TConverter.ScoreToString(AScore: Integer): String;
+var
+  Prefix: Integer;
 begin
-  Result := '';
+  if Memory.Options.Theme = THEME_MODERN then
+    Result := '%.7d'.Format([AScore])
+  else
+  begin
+    Result := '%.6d'.Format([AScore]);
+
+    if Result.Length > 6 then
+    begin
+      Prefix := Result.Substring(0, 2).ToInteger();
+      Result := Result.Remove(0, 2);
+      Result := Result.Insert(0, Chr(Prefix + Ord('A') - 10));
+    end;
+  end;
 end;
 
 
 function TConverter.LinesToString(ALines: Integer): String;
 begin
-  Result := '';
+  if Memory.Options.Theme = THEME_MODERN then
+    Result := ALines.ToString()
+  else
+    Result := '%.3d'.Format([ALines]);
 end;
 
 
 function TConverter.LevelToString(ALevel: Integer): String;
 begin
-  Result := '';
+  if Memory.Options.Theme = THEME_MODERN then
+    Result := ALevel.ToString()
+  else
+    Result := '%.2d'.Format([ALevel]);
 end;
 
 
 function TConverter.BurnedToString(ABurned: Integer): String;
 begin
-  Result := '';
+  Result := ABurned.ToString();
 end;
 
 
 function TConverter.TetrisesToString(ATetrises: Integer): String;
 begin
-  Result := '';
+  Result := ATetrises.ToString() + '%';
 end;
 
 
 function TConverter.GainToString(AGain: Integer): String;
 begin
-  Result := '';
+  Result := AGain.ToString();
 end;
 
 
