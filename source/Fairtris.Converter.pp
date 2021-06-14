a92b82c5cfa2ff5a4b317aff838d42ada20af308
Jarosław Baran <furiousprogramming@gmail.com>
2021-06-15 00:59:54 +0200

    Generated default bodies of the TConverter methods

--- source/Fairtris.Converter.pp
+++ source/Fairtris.Converter.pp
@@ -7,6 +7,14 @@ interface
 
 type
   TConverter = class(TObject)
+  public
+    function PiecesToString(APieces: Integer): String;
+    function ScoreToString(AScore: Integer): String;
+    function LinesToString(ALines: Integer): String;
+    function LevelToString(ALevel: Integer): String;
+    function BurnedToString(ABurned: Integer): String;
+    function TetrisesToString(ATetrises: Integer): String;
+    function GainToString(AGain: Integer): String;
   end;
 
 
@@ -15,6 +23,49 @@ var
 
 
 implementation
+
+
+function TConverter.PiecesToString(APieces: Integer): String;
+begin
+  Result := '';
+end;
+
+
+function TConverter.ScoreToString(AScore: Integer): String;
+begin
+  Result := '';
+end;
+
+
+function TConverter.LinesToString(ALines: Integer): String;
+begin
+  Result := '';
+end;
+
+
+function TConverter.LevelToString(ALevel: Integer): String;
+begin
+  Result := '';
+end;
+
+
+function TConverter.BurnedToString(ABurned: Integer): String;
+begin
+  Result := '';
+end;
+
+
+function TConverter.TetrisesToString(ATetrises: Integer): String;
+begin
+  Result := '';
+end;
+
+
+function TConverter.GainToString(AGain: Integer): String;
+begin
+  Result := '';
+end;
+
 
 end.
 
