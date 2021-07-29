1eb0065f85d343b8a1bad027eae8c09b634c99ac
Jarosław Baran <furiousprogramming@gmail.com>
2021-07-29 17:22:03 +0200

    Linear Feedback Shift Register implemented

--- source/Fairtris.Generators.pp
+++ source/Fairtris.Generators.pp
@@ -4,7 +4,41 @@ unit Fairtris.Generators;
 
 interface
 
+
+type
+  TShiftRegister = class(TObject)
+  private
+    FSeed: UInt16;
+  public
+    procedure Initialize();
+  public
+    procedure Reset();
+    procedure Update();
+  public
+    property Seed: UInt16 read FSeed;
+  end;
+
+
 implementation
+
+
+procedure TShiftRegister.Initialize();
+begin
+  Reset();
+end;
+
+
+procedure TShiftRegister.Reset();
+begin
+  FSeed := $8988;
+end;
+
+
+procedure TShiftRegister.Update();
+begin
+  FSeed := ((((FSeed shr 9) and 1) xor ((FSeed shr 1) and 1)) shl 15) or (FSeed shr 1);
+end;
+
 
 end.
 
