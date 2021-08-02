80305c4aa6e7f17f3c45a711b61370d0e18ce42b
Jarosław Baran <furiousprogramming@gmail.com>
2021-08-02 22:40:15 +0200

    Method to test piece position implemented

--- source/Fairtris.Core.pp
+++ source/Fairtris.Core.pp
@@ -8,10 +8,10 @@ interface
 type
   TCore = class(TObject)
   private
-    function CanPlacePiece(AX, AY: Integer): Boolean;
+    function CanPlacePiece(APieceX, APieceY: Integer): Boolean;
   private
     function CanDropPiece(): Boolean;
-    function CanShiftPiece(ADirection: Intger): Boolean;
+    function CanShiftPiece(ADirection: Integer): Boolean;
     function CanRotatePiece(ADirection: Integer): Boolean;
   public
     procedure Reset();
@@ -24,6 +24,11 @@ var
 
 
 implementation
+
+uses
+  Fairtris.Memory,
+  Fairtris.Arrays,
+  Fairtris.Constants;
 
 
 procedure TCore.Reset();
@@ -38,9 +43,27 @@ begin
 end;
 
 
-function TCore.CanPlacePiece(AX, AY: Integer): Boolean;
+function TCore.CanPlacePiece(APieceX, APieceY: Integer): Boolean;
+var
+  LayoutX, LayoutY: Integer;
 begin
+  for LayoutY := -2 to 2 do
+  begin
+    if APieceY + LayoutY < -2 then Continue;
+    if APieceY + LayoutY > 19 then Continue;
 
+    for LayoutX := -2 to 2 do
+    begin
+      if APieceX + LayoutX < 0 then Continue;
+      if APieceX + LayoutX > 9 then Continue;
+
+      if PIECE_LAYOUT[Memory.Game.PieceID, Memory.Game.PieceOrientation][LayoutY, LayoutX] <> BRICK_EMPTY then
+        if Memory.Game.Stack[APieceX + LayoutX, APieceY + LayoutY] <> BRICK_EMPTY then
+          Exit(False);
+    end;
+  end;
+
+  Result := True;
 end;
 
 
@@ -50,7 +73,7 @@ begin
 end;
 
 
-function TCore.CanShiftPiece(ADirection: Intger): Boolean;
+function TCore.CanShiftPiece(ADirection: Integer): Boolean;
 begin
 
 end;
