6066000d164936af2b023445522cae7b4c918b21
Jarosław Baran <furiousprogramming@gmail.com>
2021-08-02 22:19:07 +0200

    Added frames of basic methods to test piece place and movement

--- source/Fairtris.Core.pp
+++ source/Fairtris.Core.pp
@@ -7,6 +7,12 @@ interface
 
 type
   TCore = class(TObject)
+  private
+    function CanPlacePiece(AX, AY: Integer): Boolean;
+  private
+    function CanDropPiece(): Boolean;
+    function CanShiftPiece(ADirection: Intger): Boolean;
+    function CanRotatePiece(ADirection: Integer): Boolean;
   public
     procedure Reset();
     procedure Update();
@@ -27,6 +33,30 @@ end;
 
 
 procedure TCore.Update();
+begin
+
+end;
+
+
+function TCore.CanPlacePiece(AX, AY: Integer): Boolean;
+begin
+
+end;
+
+
+function TCore.CanDropPiece(): Boolean;
+begin
+
+end;
+
+
+function TCore.CanShiftPiece(ADirection: Intger): Boolean;
+begin
+
+end;
+
+
+function TCore.CanRotatePiece(ADirection: Integer): Boolean;
 begin
 
 end;
