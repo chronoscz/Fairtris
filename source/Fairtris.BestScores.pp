81f66d840c8c662aba044997332ca80d2173e67a
Jarosław Baran <furiousprogramming@gmail.com>
2021-08-09 00:16:06 +0200

    Implemented custom class to represent best score entry

--- source/Fairtris.BestScores.pp
+++ source/Fairtris.BestScores.pp
@@ -4,6 +4,28 @@ unit Fairtris.BestScores;
 
 interface
 
+uses
+  FGL,
+  IniFiles;
+
+
+type
+  TCustomScore = class(TObject)
+  private
+    FLinesCleared: Integer;
+    FLevelBegin: Integer;
+    FLevelEnd: Integer;
+    FTetrisRate: Integer;
+    FTotalScore: Integer;
+  protected
+    FValid: Boolean;
+  protected
+    procedure Validate(); virtual;
+  public
+    procedure Load(AFile: TIniFile; const ASection: String);
+    procedure Save(AFile: TIniFile; const ASection: String);
+  end;
+
 
 type
   TBestScores = class(TObject)
@@ -18,6 +40,50 @@ var
 
 
 implementation
+
+uses
+  Math,
+  Fairtris.Constants;
+
+
+procedure TScoreEntry.Validate();
+begin
+  FValid := InRange(FLinesCleared, 0, 999);
+
+  FValid := FValid and InRange(FLevelBegin, LEVEL_FIRST, LEVEL_LAST);
+  FValid := FValid and InRange(FLevelEnd,   LEVEL_FIRST, 99);
+
+  FValid := FValid and (FLevelBegin <= FLevelEnd);
+
+  FValid := FValid and InRange(FTetrisRate, 0, 100);
+  FValid := FValid and InRange(FTotalScore, 0, 9999999);
+end;
+
+
+procedure TScoreEntry.Load(AFile: TIniFile; const ASection: String);
+begin
+  FLinesCleared := AFile.ReadInteger(ASection, BEST_SCORES_KEY_SCORE_LINES_CLEARED, -1);
+
+  FLevelBegin := AFile.ReadInteger(ASection, BEST_SCORES_KEY_SCORE_LEVEL_BEGIN, -1);
+  FLevelEnd   := AFile.ReadInteger(ASection, BEST_SCORES_KEY_SCORE_LEVEL_END,   -1);
+
+  FTetrisRate := AFile.ReadInteger(ASection, BEST_SCORES_KEY_SCORE_TETRIS_RATE, -1);
+  FTotalScore := AFile.ReadInteger(ASection, BEST_SCORES_KEY_SCORE_TOTAL_SCORE, -1);
+
+  Validate();
+end;
+
+
+procedure TScoreEntry.Save(AFile: TIniFile; const ASection: String);
+begin
+  AFile.WriteInteger(ASection, BEST_SCORES_KEY_SCORE_LINES_CLEARED, FLinesCleared);
+
+  AFile.WriteInteger(ASection, BEST_SCORES_KEY_SCORE_LEVEL_BEGIN, FLevelBegin);
+  AFile.WriteInteger(ASection, BEST_SCORES_KEY_SCORE_LEVEL_END,   FLevelEnd);
+
+  AFile.WriteInteger(ASection, BEST_SCORES_KEY_SCORE_TETRIS_RATE, FTetrisRate);
+  AFile.WriteInteger(ASection, BEST_SCORES_KEY_SCORE_TOTAL_SCORE, FTotalScore);
+end;
 
 
 procedure TBestScores.Load();
