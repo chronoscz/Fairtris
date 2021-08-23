c616e5e94ddd89b9ffd7683919e6bbf4dcbfd421
Jarosław Baran <furiousprogramming@gmail.com>
2021-08-23 21:29:34 +0200

    Base window code implemented

--- source/Fairtris.Window.pp
+++ source/Fairtris.Window.pp
@@ -4,9 +4,21 @@ unit Fairtris.Window;
 
 interface
 
+uses
+  SDL2;
+
 
 type
   TGameForm = class(TObject)
+  private
+    FWindow: PSDL_Window;
+    FRenderer: PSDL_Renderer;
+  public
+    constructor Create();
+    destructor Destroy(); override;
+  public
+    property Window: PSDL_Window read FWindow;
+    property Renderer: PSDL_Renderer read FRenderer;
   end;
 
 
@@ -15,6 +27,25 @@ var
 
 
 implementation
+
+
+constructor TGameForm.Create();
+begin
+  FWindow := SDL_CreateWindow('Fairtris', SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_BORDERLESS);
+  if FWindow = nil then Halt();
+
+  FRenderer := SDL_CreateRenderer(FWindow, -1, SDL_RENDERER_ACCELERATED or SDL_RENDERER_TARGETTEXTURE);
+  if FRenderer = nil then Halt();
+end;
+
+
+destructor TGameForm.Destroy();
+begin
+  SDL_DestroyWindow(FWindow);
+  SDL_DestroyRenderer(FRenderer);
+
+  inherited Destroy();
+end;
 
 
 end.
