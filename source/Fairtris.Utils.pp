df487b1313eb9645ef28d8f931749f4701a80fc3
Jarosław Baran <furiousprogramming@gmail.com>
2021-08-25 00:03:32 +0200

    Added functions to extract color components

--- source/Fairtris.Utils.pp
+++ source/Fairtris.Utils.pp
@@ -16,6 +16,10 @@ type
 
   function WrapAround(AValue, ACount, AStep: Integer): Integer;
 
+  function GetR(AColor: Integer): UInt8;
+  function GetG(AColor: Integer): UInt8;
+  function GetB(AColor: Integer): UInt8;
+
 
 implementation
 
@@ -32,6 +36,24 @@ end;
 function WrapAround(AValue, ACount, AStep: Integer): Integer;
 begin
   Result := (AValue + ACount + AStep) mod ACount;
+end;
+
+
+function GetR(AColor: Integer): UInt8;
+begin
+  Result := AColor and $000000FF;
+end;
+
+
+function GetG(AColor: Integer): UInt8;
+begin
+  Result := (AColor and $0000FF00) shr 8;
+end;
+
+
+function GetB(AColor: Integer): UInt8;
+begin
+  Result := (AColor and $00FF0000) shr 16;
 end;
 
 
