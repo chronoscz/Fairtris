1b4276c6e8a62da91b8b25e44d283f5a8f1a7950
Jarosław Baran <furiousprogramming@gmail.com>
2021-08-24 22:09:03 +0200

    Added obtaining window handle and a property returning it
    The window handle is needed to manipulate the taskbar button.

--- source/Fairtris.Window.pp
+++ source/Fairtris.Window.pp
@@ -5,12 +5,14 @@ unit Fairtris.Window;
 interface
 
 uses
-  SDL2;
+  SDL2,
+  SysUtils;
 
 
 type
   TWindow = class(TObject)
   private
+    FHandle: THandle;
     FWindow: PSDL_Window;
     FRenderer: PSDL_Renderer;
   private
@@ -24,6 +26,8 @@ type
   public
     property Window: PSDL_Window read FWindow;
     property Renderer: PSDL_Renderer read FRenderer;
+  public
+    property Handle: THandle read FHandle;
   public
     property Left: Integer read GetLeft;
     property Top: Integer read GetTop;
@@ -40,12 +44,19 @@ implementation
 
 
 constructor TWindow.Create();
+var
+  SysInfo: TSDL_SysWMInfo;
 begin
   FWindow := SDL_CreateWindow('Fairtris', SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_BORDERLESS);
   if FWindow = nil then Halt();
 
   FRenderer := SDL_CreateRenderer(FWindow, -1, SDL_RENDERER_ACCELERATED or SDL_RENDERER_TARGETTEXTURE);
   if FRenderer = nil then Halt();
+
+  SDL_Version(SysInfo.Version);
+  SDL_GetWindowWMInfo(FWindow, @SysInfo);
+
+  FHandle := SysInfo.Win.Window;
 end;
 
 
