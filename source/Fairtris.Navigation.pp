20b81f4d8a10f7fd9c7223405165bf7ad632ce7e
Jarosław Baran <furiousprogramming@gmail.com>
2021-09-06 09:53:41 +0200

    Header comment added

--- source/Fairtris.Navigation.pp
+++ source/Fairtris.Navigation.pp
@@ -1,3 +1,20 @@
+{
+  Fairtris — a fair implementation of Classic Tetris®
+  Copyleft (ɔ) furious programming 2021. All rights reversed.
+
+  https://github.com/furious-programming/fairtris
+
+
+  This is free and unencumbered software released into the public domain.
+
+  Anyone is free to copy, modify, publish, use, compile, sell, or
+  distribute this software, either in source code form or as a compiled
+  binary, for any purpose, commercial or non-commercial, and by any means.
+
+  For more information, see "LICENSE" or "license.txt" file, which should
+  be included with this distribution. If not, check the repository.
+}
+
 unit Fairtris.Navigation;
 
 {$MODE OBJFPC}{$LONGSTRINGS ON}
