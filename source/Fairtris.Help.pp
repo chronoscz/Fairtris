6e21359b0a1814093dc71c005801253b093f4b81
Jarosław Baran <furiousprogramming@gmail.com>
2021-09-23 17:11:01 +0200

    #4 — opening a specific help page implemented

--- source/Fairtris.Help.pp
+++ source/Fairtris.Help.pp
@@ -18,12 +18,28 @@ type
 implementation
 
 uses
-  Windows;
+  Windows,
+  Fairtris.Logic,
+  Fairtris.Constants;
 
 
 procedure THelpThread.Execute();
+var
+  Address: String = 'https://github.com/furious-programming/fairtris/wiki';
 begin
-  ShellExecute(0, 'open', 'https://github.com/furious-programming/fairtris/wiki', nil, nil, SW_SHOWNORMAL);
+  case Logic.Scene.Current of
+    SCENE_MENU:        Address += '/prime-menu';
+    SCENE_PLAY:        Address += '/set-up-game';
+    SCENE_GAME_NORMAL: Address += '/gameplay';
+    SCENE_GAME_FLASH:  Address += '/gameplay';
+    SCENE_PAUSE:       Address += '/game-pause';
+    SCENE_TOP_OUT:     Address += '/game-summary';
+    SCENE_OPTIONS:     Address += '/game-options';
+    SCENE_KEYBOARD:    Address += '/set-up-keyboard';
+    SCENE_CONTROLLER:  Address += '/set-up-controller';
+  end;
+
+  ShellExecute(0, 'open', PChar(Address), nil, nil, SW_SHOWNORMAL);
   Terminate();
 end;
 
