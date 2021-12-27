2c62d314f9c4963a9265d6edb7439c3325a56321
Jarosław Baran <furiousprogramming@gmail.com>
2021-12-27 22:11:12 +0100

    Temporarily commented out wiki page opening
    This code will be updated at the very end when the number of scenes is known and the GitHub wiki is adapted to them.

--- source/Fairtris.Help.pp
+++ source/Fairtris.Help.pp
@@ -41,23 +41,23 @@ uses
 
 
 procedure THelpThread.Execute();
-var
-  Address: String = 'https://github.com/furious-programming/fairtris/wiki';
+//var
+//  Address: String = 'https://github.com/furious-programming/fairtris/wiki';
 begin
-  case Logic.Scene.Current of
-    SCENE_MENU:        Address += '/prime-menu';
-    SCENE_PLAY:        Address += '/set-up-game';
-    SCENE_GAME_NORMAL: Address += '/gameplay';
-    SCENE_GAME_FLASH:  Address += '/gameplay';
-    SCENE_PAUSE:       Address += '/game-pause';
-    SCENE_TOP_OUT:     Address += '/game-summary';
-    SCENE_OPTIONS:     Address += '/game-options';
-    SCENE_KEYBOARD:    Address += '/set-up-keyboard';
-    SCENE_CONTROLLER:  Address += '/set-up-controller';
-  end;
-
-  ShellExecute(0, 'open', PChar(Address), nil, nil, SW_SHOWNORMAL);
-  Terminate();
+  //case Logic.Scene.Current of
+  //  SCENE_MENU:        Address += '/prime-menu';
+  //  SCENE_PLAY:        Address += '/set-up-game';
+  //  SCENE_GAME_NORMAL: Address += '/gameplay';
+  //  SCENE_GAME_FLASH:  Address += '/gameplay';
+  //  SCENE_PAUSE:       Address += '/game-pause';
+  //  SCENE_TOP_OUT:     Address += '/game-summary';
+  //  SCENE_OPTIONS:     Address += '/game-options';
+  //  SCENE_KEYBOARD:    Address += '/set-up-keyboard';
+  //  SCENE_CONTROLLER:  Address += '/set-up-controller';
+  //end;
+  //
+  //ShellExecute(0, 'open', PChar(Address), nil, nil, SW_SHOWNORMAL);
+  //Terminate();
 end;
 
 
